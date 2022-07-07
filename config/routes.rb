343ec8cc49093dae0859a4deef56a273bfdfa1b7
lisandroseia <lisandroseia11@gmail.com>
2022-07-06 21:26:17 -0300

    Finish styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,19 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :pages, only: %i[index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'categories#index'
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  unauthenticated :user do
+    root 'pages#index', as: :unauthenticated_root
+  end
+
   resources :categories, only: %i[index show new create] do
     resources :payments, only: %i[index new create]
   end
