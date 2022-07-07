0c4d41f5a4299bbcba52a1faf432b10312271911
lisandroseia <lisandroseia11@gmail.com>
2022-07-06 21:35:09 -0300

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class Category < ActiveRecord::Base
+  validates :name, presence:true, length: {minimum: 4, maximum: 30}
+  
   belongs_to :user
   has_many :operations
   has_many :payments, through: :operations
   has_one_attached :icon
+
 
   def total
     payments.sum(:amount)
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Payment < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 4, maximum: 30 }
+  validates :name, presence: true, numericality: { only_integer: true }
+
   belongs_to :author, class_name: 'User'
   has_many :operations
   has_many :categories, through: :operations
