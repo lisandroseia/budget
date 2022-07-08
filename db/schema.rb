3fb0640eeaacdc91c9eda45b0cefd48c50d71bc7
lisandroseia <lisandroseia11@gmail.com>
2022-07-08 19:54:32 -0300

    Changed sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_01_022948) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,15 +45,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_022948) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "operations", force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "payment_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "payment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_operations_on_category_id"
@@ -60,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_022948) do
   create_table "payments", force: :cascade do |t|
     t.string "name"
     t.integer "amount"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_payments_on_author_id"
@@ -75,7 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_01_022948) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index "\"confirmation_token\"", name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
